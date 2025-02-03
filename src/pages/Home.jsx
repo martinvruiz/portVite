34539111eb55361d962407ea691257e6b1a8233c
martinvruiz <martinvruiz10@gmail.com>
2025-02-03 20:17:24 -0300

    fixed responsive issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
         <div className="min-h-screen bg-black text-white">
             <div className="flex flex-col items-center">
                 <img src={img} alt="logo" className="w-1/4"/>
-                <h1 className="text-5xl">Martin Villalo Ruiz</h1>
-                <h2 className="text-2xl p-1 text-orange-400">FrontEnd Developer</h2>
-                <p className="text-2xl p-2 font-semibold border-b mb-2">Based in Buenos Aires, Argentina.</p>
-                <p className="w-2/4 text-center text-xl">I am a front-end developer with strong skills in React, Tailwind CSS, and modern web design. I excel at creating clean, functional, and user-friendly interfaces, focusing on responsive design and ensuring a seamless user experience.</p>
+                <h1 className="lg:text-5xl text-2xl">Martin Villalo Ruiz</h1>
+                <h2 className="lg:text-2xl text-xl p-1 text-orange-400">FrontEnd Developer</h2>
+                <p className="lg:text-2xl text-lg p-2 font-semibold border-b mb-2">Based in Buenos Aires, Argentina.</p>
+                <p className="w-2/4 text-center lg:text-xl text-md">I am a front-end developer with strong skills in React, Tailwind CSS, and modern web design. I excel at creating clean, functional, and user-friendly interfaces, focusing on responsive design and ensuring a seamless user experience.</p>
             </div>
             <Lenguages/>
         </div>
