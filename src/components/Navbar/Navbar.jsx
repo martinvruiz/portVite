8e3255e3c28babbfcea8b8053f7db1b100901aa1
martinvruiz <martinvruiz10@gmail.com>
2025-02-12 16:22:29 -0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
 return (
     <nav className="w-full sticky top-0 bg-black text-white p-3">
-        <ul className="flex justify-center items-center space-x-4 text-xl">
+        <ul className="flex justify-center items-center space-x-4 md:text-2xl text-xl">
             <Link className="hover:scale-105 p-1 border-b" to="./">Home</Link>
             <Link className="hover:scale-105 p-1 border-b" to="/projects">Projects</Link>
             <Link className="hover:scale-105 p-1 border-b" to="/contact">Contact</Link>
